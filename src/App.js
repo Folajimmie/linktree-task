1f738c864ac3b931eb4886eb39f0b3179dc12eaf
Folajimmie <hayodele24@gmail.com>
2022-11-04 18:06:26 +0100

    fix: routing bug

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Timeline from "./components/Timeline";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Contact from "./components/contact";
 
 function App() {
   return (
-    <Router>
       <Routes>
         <Route path='/' element={<Timeline />}/>
         <Route path='/contact' element={<Contact />}/>
       </Routes>
-    </Router>
   );
 }
 
